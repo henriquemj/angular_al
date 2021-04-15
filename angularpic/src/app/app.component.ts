a4d88cf4d56d65506683d853f21e6b042797fd71
Henrique Madureira de Jesus <henrique.jesus@concert.com.br>
2021-04-15 20:37:48 -0300

    photo component

--- angularpic/src/app/app.component.ts
+++ angularpic/src/app/app.component.ts
@@ -6,8 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
-  title = 'angularpic';
-  description='Dohko-de-Libra';
-  url='https://i.pinimg.com/originals/5a/a2/2c/5aa22c869adb0e49b02a8019bb6c470b.jpg';
 }
---
+++ angularpic/src/app/photo/photo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'ap-photo',
+    templateUrl: 'photo.component.html'
+})
+export class PhotoComponent {
+
+    description='Dohko-de-Libra';
+    
+    url='https://i.pinimg.com/originals/5a/a2/2c/5aa22c869adb0e49b02a8019bb6c470b.jpg';
+}
