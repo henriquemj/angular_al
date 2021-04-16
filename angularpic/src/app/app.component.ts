4e26e2f6b928a2c1fe1ed0c72c348e4d28c503c9
Henrique Madureira de Jesus <henrique.jesus@concert.com.br>
2021-04-16 20:24:29 -0300

    Pasta de Photos - photos.ts

--- angularpic/src/app/app.component.ts
+++ angularpic/src/app/app.component.ts
@@ -6,4 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  photos = [
+    {
+      url: 'https://i.pinimg.com/originals/5a/a2/2c/5aa22c869adb0e49b02a8019bb6c470b.jpg',
+      description: 'Dohko-de-Libra'
+    },
+    {
+      url: 'https://images6.fanpop.com/image/photos/40100000/Libra-Dohko-saint-seiya-knights-of-the-zodiac-40123939-640-800.jpg',
+      description: 'Dohko-MestreAncião'
+    }
+  ];
+
 }
