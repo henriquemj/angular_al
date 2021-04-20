0d64690f6e71f9ea998322e3d5bf478bf8dd4cad
Henrique Madureira <39204185+henriquemj@users.noreply.github.com>
2021-04-20 00:18:51 -0300

    OnInit

--- angularpic/src/app/app.component.ts
+++ angularpic/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { PhotoService } from './photos/photo/photo.service';
 
@@ -7,14 +7,16 @@ import { PhotoService } from './photos/photo/photo.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  photos: any[] = [];  
+  photos: any[] = [];
 
-  constructor (photoService: PhotoService) {
+  constructor (private photoService: PhotoService) { }
 
-    photoService
-    .listFromUser('flavio')
-    .subscribe(photos => this.photos = photos);
+  ngOnInit(): void {
+
+    this.photoService
+      .listFromUser('flavio')
+      .subscribe(photos => this.photos = photos);  
   }
 }
--- angularpic/src/app/photos/photos.module.ts
+++ angularpic/src/app/photos/photos.module.ts
@@ -1,9 +1,11 @@
+import { HttpClientXsrfModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 
 import { PhotoComponent } from "./photo/photo.component";
 
 @NgModule({
     declarations: [ PhotoComponent ],
-    exports: [ PhotoComponent ] 
+    exports: [ PhotoComponent ] ,
+    imports: [HttpClientXsrfModule]
 })
 export class PhotosModule {}
