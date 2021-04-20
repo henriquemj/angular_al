32e0dc63f62ad7bbb8ad523549dfea06c85c0e4a
Henrique Madureira <39204185+henriquemj@users.noreply.github.com>
2021-04-19 21:57:13 -0300

    alteração das dependencias do npm/angular

--- angularpic/src/app/photos/photos.module.ts
+++ angularpic/src/app/photos/photos.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from "@angular/core";
-import { PhotoComponent } from "./photo/photo.component";
 
+import { PhotoComponent } from "./photo/photo.component";
 
 @NgModule({
     declarations: [ PhotoComponent ],
-    exports: [ PhotoComponent ]
+    exports: [ PhotoComponent ] 
 })
 export class PhotosModule {}
