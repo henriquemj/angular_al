d4818072ec7a14494ea08a5b700df8893bd95a47
Henrique Madureira <henriquemadureira05@gmail.com>
2021-04-27 22:41:04 -0300

    Parametrizando rotas / Errors

--- angularpic/src/app/app.routing.module.ts
+++ angularpic/src/app/app.routing.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { NotFoundComponent } from "./errors/not-found/not-found.component";
 
 import { PhotoFormComponent } from "./photos/photo-form/photo-form.component";
 import { PhotoListComponent } from "./photos/photo-list/photo-list.component";
 
 
 const routes: Routes = [
-    { path: 'user/flavio', component: PhotoListComponent},
-    { path: 'p/add', component: PhotoFormComponent }
+    { path: 'user/userName', component: PhotoListComponent},
+    { path: 'p/add', component: PhotoFormComponent },
+    { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule ({
-    imports: [ RouterModule.forRoot(routes) 
+    imports: [ 
+        RouterModule.forRoot(routes) 
     ],
     exports: [ RouterModule ]
 })
